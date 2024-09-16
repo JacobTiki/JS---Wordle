7329ad7a6545e9aaf9766a4b2b12a2303253af42
Jacob Sosa <jacobososa08@gmail.com>
2024-09-16 19:11:55 +0000

    wordle

---
+++ wordle.py
@@ -0,0 +1,29 @@
+word = "honey"
+
+default = '\033[0m'
+green = '\033[92m'
+yellow = '\033[33m'
+
+def gameloop():
+    userGuess = ""
+    for i in range(6):
+        userGuess = input("Give me a guess: ")
+        print(generateHint(userGuess))
+        if(userGuess == word):
+            print("You Won")
+            break
+
+def generateHint(guess):
+    color = default
+    hint = ""
+    for i in range(5):
+        if(guess[i] == word[i]):
+            color = green
+        elif(guess[i] in word):
+            color = yellow
+        else:
+            color = default
+        hint = hint + color + guess[i] + default
+    return hint
+
+gameloop()
